3a5c51103862ac5d197a359299a6fa5e283396b1
Marcus <30908995+mickeystix@users.noreply.github.com>
2022-02-21 12:14:41 -0600

    GUI Version
    NOT packaged obviously. Run from IDE or Python shell. Be sure to grab favicon.png if you want an icon, but it doesn't effect it either way.

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
-## This is the backend for PassGen. Lightweight but also incredibly insecure password generation. Don't use this in any form of production, in other words.
-## This is the "console" version
-
+from tkinter import *
 import string
 import random
+import os.path
 
-validresponses = ["y", "Y", "yes", "Yes", "n", "N", "no", "No"]
-positiveresponses = ["y", "Y", "yes", "Yes"]
-negativeresponses = ["n", "N", "no", "No"]
+## Window 
+root = Tk()
+root.geometry("300x160")
+root.title("PassGen")
+if os.path.exists("favicon.png"):
+    favicon = PhotoImage(file = "favicon.png")
+    root.iconphoto(True, favicon)
 
-def prompts(selector):
-    if selector == 1:
-        pstring = 'Numbers'
-    else:
-        pstring = 'Special Characters'
-    
-    answer = input("Are " + pstring + " Required? [y/n]: ")
+## Variables and defaults for data entry/collection
+cbSC = BooleanVar()
+cbNum = BooleanVar()
+cLen = IntVar()
+specrequired = False
+numrequired = False
 
-    if answer in validresponses:
-        if answer in positiveresponses:
-            return True
-        elif answer in negativeresponses:
-            return False
-    else:
-        print("Invalid response")
-        pgstart()
-       
-def pgstart():
-    credentiallength = int(input("Desired Credential Length: "))
-    specrequired = prompts(0)
-    numrequired = prompts(1)
-    pg(credentiallength, specrequired, numrequired)
-
+## Helper functions
 def getletter():
     return random.choice(string.ascii_letters)
 
@@ -40,8 +28,13 @@ def getnumber():
 def getspecial():
     return random.choice(string.punctuation)
 
+
+## Generate Credential
 def pg(credentiallength, specrequired, numrequired):
-    ## print("\nRequested Length: " + str(credentiallength) + "\nSpec Characters: " + str(specrequired) + "\nNumbers: " + str(numrequired) + "\n")
+    ## Just in case we get a 0 for length, still generate something
+    if credentiallength == 0:
+        credentiallength = random.randint(8, 12)
+
     credential = ''
     i = 0
 
@@ -77,8 +70,45 @@ def pg(credentiallength, specrequired, numrequired):
             credential = credential + (getletter())
         
         i += 1
-        
-    print("Generated Credential: " + credential)
 
-if __name__ == ('__main__'):
-    pgstart()
+    ## Clear and update output
+    eOutput.delete(0, END)
+    eOutput.insert(0, credential)
+
+
+## Button click function
+def btnTest():
+    
+    if cbSC.get() == 1:
+        specrequired = True
+    else:
+        specrequired = False
+    
+    if cbNum.get() == 1:
+        numrequired = True
+    else:
+        numrequired = False
+
+    pg(cLen.get(), specrequired, numrequired)
+    
+
+## GUI Items
+lblSC = Label(root, text="Are Special Characters required?").grid(row=0, column=0)
+scChkbx = Checkbutton(root, variable=cbSC, onvalue=True, offvalue=False).grid(row=0, column=1)
+
+lblNum = Label(root, text="Are Numbers required?").grid(row=1, column=0)
+numChkbx = Checkbutton(root, variable=cbNum, onvalue=True, offvalue=False).grid(row=1, column=1)
+
+lblLength = Label(root, text="Length required?").grid(row=2, column=0)
+eLength = Entry(root, textvariable=cLen).grid(row=2, column=1)
+
+lblSpacer = Label(root, text=' ').grid(row=3, column=0)
+
+lblCred = Label(root, text="Generated Credential: ").grid(row=4, column=0)
+eOutput = Entry(root) ## grid addition has to be separated, otherwise check values return a 'None' before item is placed.
+eOutput.grid(row=4, column=1)
+
+btnGen = Button(root, text="Generate", command=btnTest).grid(row=6, column=0)
+
+
+root.mainloop()
