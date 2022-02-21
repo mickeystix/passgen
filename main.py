25502e2efb76e85b808428b273303497599092ad
Marcus <30908995+mickeystix@users.noreply.github.com>
2022-02-21 17:05:40 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from tkinter import *
 import string
 import random
 import os.path
+from tkinter import scrolledtext
 import urllib.request
+import webbrowser
 
 ## Window 
 root = Tk()
-root.geometry("300x180")
+root.geometry("675x380")
 root.title("PassGen")
 root.resizable(False, False)
 if os.path.exists("favicon.png"):
@@ -16,6 +18,7 @@ if os.path.exists("favicon.png"):
 
 ## Variables and defaults for data entry/collection
 word_url = "https://www.mit.edu/~ecprice/wordlist.10000"
+gh = "https://github.com/mickeystix/passgen"
 cbSC = BooleanVar()
 cbNum = BooleanVar()
 cbDict = BooleanVar()
@@ -154,10 +157,14 @@ def pg(credentiallength, specrequired, numrequired, dictrequired):
             credential = credential + (getletter())
             fixer -= 1
     
+    if len(credential) > credentiallength:
+        credential = credential[0:credentiallength]
+
     ## Clear and update output
     eOutput.delete(0, END)
     eOutput.insert(0, credential)
 
+    lDisplay = Label(root, text="Length Check: " + str(len(credential)), fg="grey").grid(row=3, column=11)
 
 ## Button click function
 def gen():
@@ -180,27 +187,58 @@ def gen():
     pg(cLen.get(), specrequired, numrequired, dictrequired)
     
 
+def insertText():
+    txtField.insert(END, (eOutput.get() + "\n"))
+
+def clearPad():
+    txtField.delete(1.0, END)
+
 ## GUI Items
+## Spec Char checkbox
 lblSC = Label(root, text="Are Special Characters required?").grid(row=0, column=0)
 scChkbx = Checkbutton(root, variable=cbSC, onvalue=True, offvalue=False).grid(row=0, column=1)
 
+## Numbers checkbox
 lblNum = Label(root, text="Are Numbers required?").grid(row=1, column=0)
 numChkbx = Checkbutton(root, variable=cbNum, onvalue=True, offvalue=False).grid(row=1, column=1)
 
-lblDict = Label(root, text="Use Dictionary Words?").grid(row=2, column=0)
+## Dict words checkbox
+lblDict = Label(root, text="Try Dictionary Words?").grid(row=2, column=0)
 dictChkbx = Checkbutton(root, variable=cbDict, onvalue=True, offvalue=False).grid(row=2, column=1)
+lblDictInfo = Label(root, text="Words may truncate to required length", fg="grey").grid(row=2, column=11)
 
+## Length input field
 lblLength = Label(root, text="Length required?").grid(row=3, column=0)
 eLength = Entry(root, textvariable=cLen).grid(row=3, column=1)
 
 lblSpacer = Label(root, text=' ').grid(row=4, column=0)
 lblDefault = Label(root, text="Default: 8-12", fg="grey").grid(row=4, column=1)
 
+## Generated Credential Output field
 lblCred = Label(root, text="Generated Credential: ").grid(row=5, column=0)
 eOutput = Entry(root) ## grid addition has to be separated, otherwise check values return a 'None' before item is placed.
 eOutput.grid(row=5, column=1)
 
+## Generate Credential Button
 btnGen = Button(root, text="Generate", command=gen).grid(row=6, column=1)
+
+## Insert generated password into Text Area Button
+btnInsert = Button(root, text="Insert to Notepad", command=insertText).grid(row=9, column=10)
+
+## Clear Text Area Button
+btnClearPad = Button(root, text="Clear Notepad", command=clearPad).grid(row=9, column=11)
+
+lblSpacer = Label(root, text=' ').grid(row=8, column=0)
+
+## Scratchpad w/scrollbar
+txtFrame = Frame(root).grid(row=10, column=0)
+txtField = scrolledtext.ScrolledText(txtFrame, height=10)
+txtField.grid(row=10, column=0, columnspan=12, padx=(10, 0))
+
+## Link to Github
+lblAbout = Label(root, text="View on GitHub", fg="blue", cursor="hand2")
+lblAbout.grid(row=0, column=11)
+lblAbout.bind("<Button-1>", lambda event: webbrowser.open(gh))
 
 
 root.mainloop()
